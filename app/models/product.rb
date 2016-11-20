e015fb5ebcad0947d4303c8e8d5c12335b728ac4
Huy Vo <huyvohcmc@gmail.com>
2016-11-20 16:26:22 +0700

    ...

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,6 @@ class Product < ApplicationRecord
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+  validates :title, length: { minimum: 10 }, too_short: 'Your title must be at least 10 characters'
+
 end
